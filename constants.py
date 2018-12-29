1c130c1be713d9b6a2d0e5bd3ddf29af20053f93
Parker Watts <pwatts777@gmail.com>
2018-12-28 20:29:25 -0800

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,10 @@
+# Constants
+R = 10973731.568508 # 1/m 
+h = 6.626 * (10 ** -34) # Joule * seconds
+electron_mass = 9.10938356 * (10 ** -31) # kg
+electron_charge = 1.60217662 * (10 ** -19) # coulombs
+proton_mass = 1.6726219 * (10 ** -27) # kg 
+proton_radius = 10 ** -15 # meters
+bohr_radius = 5.29 * (10 ** -11) # meters, radius of H atom
+
+
